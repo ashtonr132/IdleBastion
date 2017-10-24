4c1ff1779d57a064e23b97e781a579ece6ccadb4
ashtonr132 <ashtonr132@gmail.com>
2017-10-24 08:42:06 +0100

    ui button handler correctly functional

--- Assets/Resources/UIElements/UI/ButtonHandler.cs
+++ Assets/Resources/UIElements/UI/ButtonHandler.cs
@@ -5,45 +5,63 @@ using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
 {
-    private int alpha;
-    private string str;
-    internal static List<GameObject> buttons = new List<GameObject>();
     private void Start()
     {
-        buttons.Add(gameObject);
-        char[] chars = gameObject.name.ToCharArray(); // Name string to char array
+        foreach (Transform child in transform)
+        {
+            child.gameObject.GetComponent<Button>().onClick.AddListener(ToggleUI); //Add ref to onclick function
+            foreach (Transform butt in GetRelativeUI(child.gameObject).transform) //Dont Render any open ui on start
+            {
+                butt.gameObject.GetComponent<CanvasRenderer>().SetAlpha(0);
+                butt.GetChild(0).GetComponent<CanvasRenderer>().SetAlpha(0);
+            }
+            GetRelativeUI(child.gameObject).GetComponent<CanvasRenderer>().SetAlpha(0);
+        }
+    }
+    private void ToggleUI()
+    {
+        foreach (Transform child in gameObject.transform) //Each button in ui list
+        {
+            if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name == child.name) //This button
+            {
+                if (GetRelativeUI(child.gameObject).GetComponent<CanvasRenderer>().GetAlpha() == 0)
+                {
+                    foreach (Transform butt in GetRelativeUI(child.gameObject).transform) //Render this ui
+                    {
+                        butt.gameObject.GetComponent<CanvasRenderer>().SetAlpha(1);
+                        butt.GetChild(0).GetComponent<CanvasRenderer>().SetAlpha(1);
+                    }
+                    GetRelativeUI(child.gameObject).GetComponent<CanvasRenderer>().SetAlpha(1);
+                }
+                else
+                {
+                    foreach (Transform butt in GetRelativeUI(child.gameObject).transform) //Unless its already being rendered in which case dont
+                    {
+                        butt.gameObject.GetComponent<CanvasRenderer>().SetAlpha(0);
+                        butt.GetChild(0).GetComponent<CanvasRenderer>().SetAlpha(0);
+                    }
+                    GetRelativeUI(child.gameObject).GetComponent<CanvasRenderer>().SetAlpha(0);
+                }
+            }
+            else
+            {
+                foreach (Transform butt in GetRelativeUI(child.gameObject).transform) //Dont render the other ui'
+                {
+                    butt.gameObject.GetComponent<CanvasRenderer>().SetAlpha(0);
+                    butt.GetChild(0).GetComponent<CanvasRenderer>().SetAlpha(0);
+                }
+                GetRelativeUI(child.gameObject).GetComponent<CanvasRenderer>().SetAlpha(0);
+            }
+        }
+    }
+    private GameObject GetRelativeUI(GameObject go)
+    {
+        char[] chars = go.name.ToCharArray(); //Name string to char array
         char[] charz = new char[chars.Length - 6];
-        for (int i = 0; i < charz.Length; i++) // -Button suffix
+        for (int i = 0; i < charz.Length; i++) //-Button suffix in the string
         {
             charz[i] = chars[i];
         }
-        str = new string(charz);
-        GetComponent<Button>().onClick.AddListener(UIButton);
-        if (GameObject.Find(str).GetComponent<CanvasRenderer>().GetAlpha() > 0)
-        {
-            alpha = 1;
-        }
-        else
-        {
-            alpha = 0;
-        }
-        UIButton();
-    }
-    private void UIButton()
-    {
-        if (alpha == 1)
-        {
-            alpha = 0;
-        }
-        else
-        {
-            alpha = 1;
-        }
-        foreach (Transform transform in GameObject.Find(str).transform)
-        {
-            transform.gameObject.GetComponent<CanvasRenderer>().SetAlpha(alpha);
-            transform.GetChild(0).GetComponent<CanvasRenderer>().SetAlpha(alpha);
-        }
-        GameObject.Find(str).GetComponent<CanvasRenderer>().SetAlpha(alpha);
+        return GameObject.Find(new string(charz));
     }
 }
